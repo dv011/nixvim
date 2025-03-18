fa8eb12a1e8cbc2ea989573843b36ce5345eec0d
Daniel Vasquez <102821982+dv011@users.noreply.github.com>
2025-03-18 16:33:37 -0400

    sigh i reset everything by accident

--- config/utils/render-markdown.nix
+++ config/utils/render-markdown.nix
@@ -1,5 +1,25 @@
 {
   plugins.render-markdown = {
     enable = true;
+    settings = {
+      code = {
+        # Turn on / off any sign column related rendering
+        sign = false;
+        # Width of the code block background:
+        #  block: width of the code block
+        #  full:  full width of the window
+        width = "block";
+        min_width = 45;
+        left_pad = 1;
+        right_pad = 1;
+        language_pad = 1;
+        # Determines how code blocks & inline code are rendered:
+        #  none:     disables all rendering
+        #  normal:   adds highlight group to code blocks & inline code, adds padding to code blocks
+        #  language: adds language icon to sign column if enabled and icon + name above code blocks
+        #  full:     normal + language
+        style = "full";
+      };
+    };
   };
 }
